3506328b92e1788688dd62b2d59bcb4a8f34a7a8
YeeChuen <tychuen98@gmail.com>
2023-04-07 15:29:30 -0500

    1st try update on heu-agent

--- heur_test.py
+++ heur_test.py
@@ -7,15 +7,18 @@ import time
 board = Board()
 agent_color = BLACK
 other_color = WHITE
-agent = Heu_Agent(color=agent_color)
-other = Rand_Agent()
+agent = Rand_Agent()
+other = Heu_Agent(color=agent_color)
 NUM_EPISODES = 100
 
+global move
 while True:
     counter = 3
     check = True
     board.reset()
     print("new game")
+    board.print_board()
+    print("----------------------------")
     while check: 
         if board.game_ended():
             break
@@ -35,12 +38,17 @@ while True:
             counter += 1
             continue
         if color == WHITE:        
-            move = other.rand_move(legal_moves)
+            move = agent.rand_move(legal_moves)
         else: # else randome
-            move = agent.heu_move(current_state,legal_moves)
+            move = other.heu_move(current_state,legal_moves)
 
+        print("--- information on moves ---")
+        print(type(move))
+        print(move)
+        print("--- end on moves ---")
         reward = board.play(move,color)
-        print(color)
+        print("color: {}".format(str(color)))
+        print("updated board:")
         board.print_board()
         print()
         counter += 1
